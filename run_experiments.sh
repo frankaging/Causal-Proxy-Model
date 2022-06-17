0df9f0c1d7036fdccbbbad36e56e257f2c0cc0aa
frankaging <zxw0424@gmail.com>
2022-06-17 16:23:38 -0700

    update notebook with grid run

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,4 +1,4 @@
-for h_dim in 4 16 32 64; do # 192; do
+for h_dim in 128; do # 4 16 32 64; do
     for class_num in 2 3 5; do
         for seed in 42; do
             CUDA_VISIBLE_DEVICES=0,6,8,9 python Proxy_training.py \
@@ -30,3 +30,37 @@ for h_dim in 4 16 32 64; do # 192; do
         done
     done
 done
+
+# control!
+# for h_dim in 192; do # 4 16 32 64; do
+#     for class_num in 2 3 5; do
+#         for seed in 42; do
+#             CUDA_VISIBLE_DEVICES=0,6,8,9 python Proxy_training.py \
+#             --tokenizer_name bert-base-uncased \
+#             --model_name_or_path ./saved_models/bert-base-uncased.opentable.CEBaB.sa.${class_num}-class.exclusive.seed_${seed}/ \
+#             --high_level_model_type_or_path ./saved_models/bert-base-uncased.opentable.CEBaB.sa.${class_num}-class.exclusive.seed_${seed}/ \
+#             --task_name CEBaB \
+#             --dataset_name ./datasets/Proxy.CEBaB.sa.${class_num}-class.exclusive \
+#             --do_train \
+#             --do_eval \
+#             --max_seq_length 128 \
+#             --per_device_train_batch_size 32 \
+#             --per_device_eval_batch_size 32 \
+#             --learning_rate 8e-5 \
+#             --num_train_epochs 60 \
+#             --output_dir ./proxy_training_results/ \
+#             --cache_dir ./train_cache/ \
+#             --seed ${seed} \
+#             --report_to wandb \
+#             --wandb_metadata wuzhengx:Causal-Proxy-Model \
+#             --logging_steps 1 \
+#             --alpha 1.0 \
+#             --beta 0.0 \
+#             --gemma 0.0 \
+#             --overwrite_output_dir \
+#             --intervention_h_dim ${h_dim} \
+#             --classifier_dropout 0.1 \
+#             --encoder_dropout 0.1
+#         done
+#     done
+# done
